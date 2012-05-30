f174169989add24158bbe83242262a3b9eff9ff3
bgbotond <bgbotond@gmail.com>
2012-05-30 12:37:11 +0300

    new xml parameters

--- include/AreaController.h
+++ include/AreaController.h
@@ -2,9 +2,6 @@
 #include "Area.h"
 #include "cinder/app/MouseEvent.h"
 
-using namespace ci;
-using namespace ci::app;
-
 namespace TouchMovie
 {
 
@@ -14,25 +11,34 @@ public:
 	AreaController();
 	void update();
 	void draw();
-	void mouseMove( MouseEvent event );
-	void mouseDown( MouseEvent event );
-	void mouseUp  ( MouseEvent event );
+	void mouseMove( ci::app::MouseEvent event );
+	void mouseDown( ci::app::MouseEvent event );
+	void mouseUp  ( ci::app::MouseEvent event );
 
-	void addArea( std::string name, Rectf &rect );
+	void addArea( std::string name, ci::Rectf &rect );
 	void removeArea( std::string name );
 	void setMain( std::string name );
-	bool setMovie( std::string name, fs::path pathMovie );
-	void setRect( std::string name, Rectf &rect );
+	bool setMovie( std::string name, ci::fs::path pathMovie );
+	void setRect( std::string name, ci::Rectf &rect );
 	void setAlpha( std::string name, float alpha );
 
 	const int getWidth ( std::string name );
 	const int getHeigth( std::string name );
 
+	void setDrawFrame( std::string name, bool drawFrame );
+	bool getDrawFrame( std::string name );
+
+	void setFadeIn( std::string name, float fadeIn );
+	float getFadeIn( std::string name );
+
+	void setFadeOut( std::string name, float fadeOut );
+	float getFadeOut( std::string name );
+
 	Area *getAreaMain();
 
 private:
 	Area *_getArea( std::string name );
-	Area *_getArea( const Vec2i &pos );
+	Area *_getArea( const ci::Vec2i &pos );
 
 private:
 
